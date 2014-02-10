7baf219ed7c2e7f958548f383d3b0723b22064c7
Casey <caseylefevrewaters@gmail.com>
2014-02-09 19:13:18 -0800

    And now it works OK.

--- main.js
+++ main.js
@@ -1,74 +1,75 @@
 var timer = {
-	delayed: true,
-	queuedDisplay: false,
-	running: [],
-	counts: {},
-	blocked: [],
-	log: [],
 	reset: function(){
+		this.stack = [];
+		this.runs = {};
 		this.running = [];
-		this.counts = {};
 		this.blocked = [];
 		this.log = [];
+		this.delayed = true;
+		return this;
 	},
 	set: function(name, over, block){
 		name = name || "anonymous";
-		if(this.blocked.indexOf(name) !== -1 || this.delayed){
-			return;
-		}
-		this.running.push({
-			name: name,
-			over: over,
-			start: Date.now(),
-			block: block
-		});
-		var countID = name;
-		if(over !== undefined && this.counts[countID] === undefined){
-			this.counts[countID] = {
-				runs: []
-			};
-		}
-	},
-	finish: function(name){
-		if(this.blocked.indexOf(name) !== -1 || this.delayed){
+		var that = this;
+		if(that.blocked.indexOf(name) !== -1 || that.delayed){
 			return;
 		}
 		
-		var result = this.running.pop();
+		that.stack.push({
+			name: name,
+			over: over,
+			start: performance.now(),
+			block: block
+		});
+		var runID = name;
+		if(over !== undefined && that.runs[runID] === undefined){
+			that.runs[runID] = [];
+			that.running.push(runID);
+		}
+	},
+	finish: function(name){
+		var that = this;
+		if(that.blocked.indexOf(name) !== -1 || that.delayed){
+			return;
+		}
+		
+		var result = that.stack.pop();
 		if(result === undefined){
 			return;
 		}
-		var delta = Date.now()-result.start;
-		//console.assert(name === undefined || result.name === name);
+		var delta = performance.now()-result.start;
 		if(result.over !== undefined){
 			var countID = name;
-			this.counts[countID].runs.push(delta);
-			if(this.counts[countID].runs.length >= result.over){
-				var runs = this.counts[countID].runs;
+			that.runs[countID].push(delta);
+			if(that.runs[countID].length >= result.over){
+				var runs = that.runs[countID];
 				for(var i = 1, sum = runs[0]; i < runs.length; i += 1){sum += runs[i];}
 				var average = sum/i;
-				this.log.push(name + " (average over " + result.over + " runs): " + average + "ms");
-				console.log(this.log[this.log.length-1]);
-				this.displayResults();
+				that.log.push(name + " (average over " + result.over + " runs): " + average + "ms");
+				
+				that.runs[countID] = [];
+				that.running.splice(that.running.indexOf(countID), 1);
+				that.displayResults();
 				
 				if(result.block === true){
-					this.blocked.push(name);
+					that.blocked.push(name);
 				}
 			}
 		}else{
-			this.log.push(name + ": " + delta + "ms");
-			console.log(this.log[this.log.length-1]);
-			this.displayResults();
+			that.log.push(name + ": " + delta + "ms");
+			that.displayResults();
 		}
 	},
 	displayResults: function(){
 		var that = this;
-		if(!that.queuedDisplay){
+		if(that.running.length === 0 && !that.queuedDisplay){
 			that.queuedDisplay = true;
 			window.setTimeout(function(){
 				alert(that.log.join("\n"));
+				that.log = [];
 				that.queuedDisplay = false;
 			}, 0);
 		}
 	}
 };
+timer.reset();
